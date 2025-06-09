c15c8577c337dcb298149698d21b3a8eabac628c
Juan Jose Zuluaga <juanzpjose136@gmail.com>
2025-06-09 17:20:21 -0500

    refactor: abstraction into Simulation class

---
+++ src/simulation.py
@@ -0,0 +1,145 @@
+from importlib import import_module
+from abc import ABC, abstractmethod
+from typing import Callable
+
+from src.solverBase import SolverBase
+import numpy as np
+from datetime import datetime, timezone, timedelta
+from mpi4py import MPI
+from ufl import inner, dx
+from dolfinx.io import VTXWriter
+from dolfinx.mesh import Mesh
+from dolfinx.fem import DirichletBC, form, assemble_scalar, Function, Expression
+
+
+class Simulation(ABC):
+    @property
+    @abstractmethod
+    def mesh(self) -> Mesh:
+        pass
+
+    @property
+    @abstractmethod
+    def bcu(self) -> list[DirichletBC]:
+        pass
+
+    @property
+    @abstractmethod
+    def bcp(self) -> list[DirichletBC]:
+        pass
+
+    @abstractmethod
+    def initial_velocity(self, x: np.ndarray) -> np.ndarray:
+        pass
+
+    def exact_velocity(self, t) -> Function | Expression | Callable:
+        pass
+
+    def __init__(
+        self, solver_name: str, simulation_name: str, rho: float, mu: float, dt: float, T: float, f: list
+    ):
+        self.solver_name = solver_name
+        self.solverClass: type[SolverBase] = getattr(
+            import_module(f"src.solvers.{solver_name}"), "Solver"
+        )
+        self.solver = self.solverClass(self.mesh, dt, rho, mu, f, self.initial_velocity)
+
+        self.num_steps = int(T / dt)
+        self.has_exact_solution = (
+            self.__class__.exact_velocity is not Simulation.exact_velocity
+        )
+
+        self.dt = dt
+        self.simulation_name = simulation_name
+
+        self.solver.assembleTimeIndependent(self.bcu, self.bcp)
+
+    def solve(self) -> None:
+        tqdm = self.get_tqdm()
+        mesh = self.mesh
+        num_steps = self.num_steps
+        solver = self.solver
+
+        progress = (
+            tqdm(desc="Resolviendo", total=num_steps) if mesh.comm.rank == 0 else None
+        )
+
+        date = (
+            datetime.now(tz=timezone(-timedelta(hours=5))).isoformat(timespec="seconds")
+            if mesh.comm.rank == 0
+            else None
+        )
+        date = mesh.comm.bcast(date, root=0)
+
+        safe_date = date.replace(":", ".")
+        parent_route = f"results/{self.simulation_name}/{self.solver_name}/{safe_date}"
+        u_file = VTXWriter(mesh.comm, f"{parent_route}/v.bp", self.solver.u_sol)
+        p_file = VTXWriter(mesh.comm, f"{parent_route}/p.bp", self.solver.p_sol)
+
+        t = 0.0
+        u_file.write(t)
+        p_file.write(t)
+
+        error_log = (
+            open(f"{parent_route}/err.txt", "w") if mesh.comm.rank == 0 else None
+        )
+
+        if self.has_exact_solution:
+            u_e = Function(solver.V)
+            u_e.interpolate(lambda x: self.exact_velocity(t)(x))
+            error = self.compute_error(solver.u_sol, u_e, mesh)
+
+        if error_log:
+            error_log.write("t = %.3f: error = %.3g" % (t, error) + "\n")
+
+        for _ in range(num_steps):
+            if progress:
+                progress.update()
+
+            solver.solveStep(self.bcu, self.bcp)
+
+            t += self.dt
+
+            if error_log:
+                u_e.interpolate(self.exact_velocity(t))
+                error = self.compute_error(u_e, solver.u_sol, mesh)
+                error_log.write("t = %.3f: error = %.3g" % (t, error) + "\n")
+
+            u_file.write(t)
+            p_file.write(t)
+
+        u_file.close()
+        p_file.close()
+        if error_log:
+            error_log.close()
+        if progress:
+            progress.close()
+
+    @staticmethod
+    def get_tqdm():
+        try:
+            # Check if inside Jupyter notebook
+            from IPython import get_ipython
+
+            shell = get_ipython().__class__.__name__
+            if shell in ["ZMQInteractiveShell"]:
+                from tqdm.notebook import tqdm as notebook_tqdm
+
+                return notebook_tqdm
+        except:
+            pass
+        from tqdm import tqdm
+
+        return tqdm
+
+    @staticmethod
+    def compute_error(u: Function, u_aprox: Function, mesh: Mesh) -> float:
+        """Compute the L2 relative error between u and u_aprox."""
+
+        error_form = form(inner(u_aprox - u, u_aprox - u) * dx)
+        error_abs = np.sqrt(
+            mesh.comm.allreduce(assemble_scalar(error_form), op=MPI.SUM)
+        )
+        norm_form = form(inner(u, u) * dx)
+        norm = np.sqrt(mesh.comm.allreduce(assemble_scalar(norm_form), op=MPI.SUM))
+        return error_abs / norm
---
+++ src/solverBase.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from typing import Callable
+import numpy as np
+from ufl import (
+    Identity,
+    nabla_grad,
+    sym,
+)
+from dolfinx.mesh import Mesh
+
+
+class SolverBase(ABC):
+    @abstractmethod
+    def __init__(
+        self,
+        domain: Mesh,
+        dt: float,
+        rho: float,
+        mu: float,
+        f: list,
+        initial_velocity: Callable[[np.ndarray], np.ndarray] = None,
+    ):
+        pass
+
+    @abstractmethod
+    def assembleTimeIndependent(self, bcu: list, bcp: list) -> None:
+        pass
+
+    @abstractmethod
+    def solveStep(self, bcu: list, bcp: list) -> None:
+        pass
+
+    @staticmethod
+    def epsilon(u):
+        return sym(nabla_grad(u))
+
+    @staticmethod
+    def sigma(u, p, mu):
+        return 2 * mu * sym(nabla_grad(u)) - p * Identity(len(u))
