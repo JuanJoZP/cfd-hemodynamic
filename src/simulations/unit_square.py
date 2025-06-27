c79b49981a313babc0662cda8669601cbc9a868b
Juan Jose Zuluaga <juanzpjose136@gmail.com>
2025-06-27 21:52:47 +0000

    format and provide velocity inlet instead of pressure (more robust)

--- src/simulations/unit_square.py
+++ src/simulations/unit_square.py
@@ -3,16 +3,22 @@ from mpi4py import MPI
 from petsc4py import PETSc
 import numpy as np
 from dolfinx.mesh import create_unit_square, locate_entities_boundary, Mesh
-from dolfinx.fem import dirichletbc, locate_dofs_topological, Constant, DirichletBC
+from dolfinx.fem import (
+    dirichletbc,
+    locate_dofs_topological,
+    Constant,
+    DirichletBC,
+    Function,
+)
 
 
-solver_name = "solver1"
+solver_name = "stabilized_schur_full"
 simulation_name = "unit_square"
 n_cells = 32
 rho = 1
 mu = 1
 dt = 1 / 200
-T = 5
+T = 2
 
 
 class UnitSquareSimulation(Simulation):
@@ -36,13 +42,20 @@ class UnitSquareSimulation(Simulation):
     @property
     def bcu(self):
         if not self._bcu:
+            uD = Function(self.solver.V)
+            uD.x.array[:] = 0
             fdim = self.mesh.topology.dim - 1
             walls_facets = locate_entities_boundary(self.mesh, fdim, self.walls)
             dofs_walls = locate_dofs_topological(self.solver.V, fdim, walls_facets)
-            bc_noslip = dirichletbc(
-                Constant(self.mesh, PETSc.ScalarType((0, 0))), dofs_walls, self.solver.V
-            )
-            self._bcu = [bc_noslip]
+            bc_noslip = dirichletbc(uD, dofs_walls)
+
+            ul = Function(self.solver.V)
+            ul.interpolate(self.exact_velocity(0))
+            inflow_facets = locate_entities_boundary(self.mesh, fdim, self.inflow)
+            dofs_inflow = locate_dofs_topological(self.solver.V, fdim, inflow_facets)
+            bc_inflow = dirichletbc(ul, dofs_inflow)
+
+            self._bcu = [bc_inflow, bc_noslip]
 
         return self._bcu
 
@@ -50,29 +63,22 @@ class UnitSquareSimulation(Simulation):
     def bcp(self):
         if not self._bcp:
             fdim = self.mesh.topology.dim - 1
-            inflow_facets = locate_entities_boundary(self.mesh, fdim, self.inflow)
-            dofs_inflow = locate_dofs_topological(self.solver.Q, fdim, inflow_facets)
-            bc_inflow = dirichletbc(
-                Constant(self.mesh, PETSc.ScalarType(8)),
-                dofs_inflow,
-                self.solver.Q,
-            )
-
+            pr = Function(self.solver.Q)
+            pr.x.array[:] = 0
             outflow_facets = locate_entities_boundary(self.mesh, fdim, self.outflow)
             dofs_outflow = locate_dofs_topological(self.solver.Q, fdim, outflow_facets)
             bc_outflow = dirichletbc(
-                Constant(self.mesh, PETSc.ScalarType(0)),
+                pr,
                 dofs_outflow,
-                self.solver.Q,
             )
 
-            self._bcp = [bc_inflow, bc_outflow]
+            self._bcp = [bc_outflow]
 
         return self._bcp
 
     def initial_velocity(self, x):
         values = np.zeros((self.mesh.geometry.dim, x.shape[1]), dtype=PETSc.ScalarType)
-        values[1] = 10
+        values[0] = 1
         return values
 
     def exact_velocity(self, t):
