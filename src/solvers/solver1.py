acf94ad704e545df787b8144e2d634b3be1a71e9
Juan Jose Zuluaga <juanzpjose136@gmail.com>
2025-07-09 14:53:24 +0000

    move constant and spaces initialization to solverBase, remove bcs from def solve()

--- src/solvers/solver1.py
+++ src/solvers/solver1.py
@@ -1,13 +1,10 @@
 # IPCS con método del punto medio y linealización del término convectivo mediante la solución en el paso anterior
 
 from typing import Callable
-
 from petsc4py import PETSc
 import numpy as np
-
-from basix.ufl import element
 from dolfinx.mesh import Mesh
-from dolfinx.fem import form, DirichletBC, Constant, Function, functionspace
+from dolfinx.fem import form, Function
 from dolfinx.fem.petsc import (
     assemble_matrix,
     assemble_vector,
@@ -41,21 +38,14 @@ class Solver(SolverBase):
         rho: float,
         mu: float,
         f: list,
-        h: list = None,
         initial_velocity: Callable[[np.ndarray], np.ndarray] = None,
     ):
-        self.mesh = mesh
-        self.dt = Constant(mesh, PETSc.ScalarType(dt))
-        self.rho = Constant(mesh, PETSc.ScalarType(rho))
-        self.mu = Constant(mesh, PETSc.ScalarType(mu))
-        self.f = Constant(mesh, PETSc.ScalarType(f))
+        super().__init__(mesh, dt, rho, mu, f, initial_velocity)
 
-        element_velocity = element(
+        super().initVelocitySpace(
             "Lagrange", mesh.topology.cell_name(), 2, shape=(mesh.geometry.dim,)
         )
-        element_pressure = element("Lagrange", mesh.topology.cell_name(), 1)
-        self.V = functionspace(mesh, element_velocity)
-        self.Q = functionspace(mesh, element_pressure)
+        super().initPressureSpace("Lagrange", mesh.topology.cell_name(), 1)
 
         u = TrialFunction(self.V)
         p = TrialFunction(self.Q)
@@ -63,14 +53,11 @@ class Solver(SolverBase):
         v = TestFunction(self.V)
         q = TestFunction(self.Q)
 
-        self.u_sol = Function(self.V)
         self.u_prev = Function(self.V)
-        self.p_sol = Function(self.Q)
         self.p_prev = Function(self.Q)
 
         if initial_velocity:
             self.u_prev.interpolate(initial_velocity)
-            self.u_sol.interpolate(initial_velocity)
 
         # weak form
         u_midpoint = 0.5 * (self.u_prev + u)
@@ -95,17 +82,15 @@ class Solver(SolverBase):
         self.a3 = form(lhs(F3))
         self.L3 = form(rhs(F3))
 
-    def assembleTimeIndependent(
-        self, bcu: list[BoundaryCondition], bcp: list[BoundaryCondition]
-    ) -> None:
-        bcu_d = [bc.getBC(self.V) for bc in bcu]
-        bcp_d = [bc.getBC(self.Q) for bc in bcp]
+    def setup(self, bcu: list[BoundaryCondition], bcp: list[BoundaryCondition]) -> None:
+        self.bcu_d = [bc.getBC(self.V) for bc in bcu]
+        self.bcp_d = [bc.getBC(self.Q) for bc in bcp]
 
-        self.A1 = assemble_matrix(self.a1, bcs=bcu_d)
+        self.A1 = assemble_matrix(self.a1, bcs=self.bcu_d)
         self.A1.assemble()
         self.b1 = create_vector(self.L1)
 
-        self.A2 = assemble_matrix(self.a2, bcs=bcp_d)
+        self.A2 = assemble_matrix(self.a2, bcs=self.bcp_d)
         self.A2.assemble()
         self.b2 = create_vector(self.L2)
 
@@ -133,19 +118,16 @@ class Solver(SolverBase):
         pc3 = self.solver3.getPC()
         pc3.setType(PETSc.PC.Type.SOR)
 
-    def solveStep(self, bcu: list[BoundaryCondition], bcp: list[BoundaryCondition]):
-        bcu_d = [bc.getBC(self.V) for bc in bcu]
-        bcp_d = [bc.getBC(self.Q) for bc in bcp]
-
+    def solveStep(self):
         # step 1
         with self.b1.localForm() as loc_1:
             loc_1.set(0)
         assemble_vector(self.b1, self.L1)
-        apply_lifting(self.b1, [self.a1], [bcu_d])
+        apply_lifting(self.b1, [self.a1], [self.bcu_d])
         self.b1.ghostUpdate(
             addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE
         )
-        set_bc(self.b1, bcu_d)
+        set_bc(self.b1, self.bcu_d)
         self.solver1.solve(self.b1, self.u_sol.x.petsc_vec)
         self.u_sol.x.scatter_forward()
 
@@ -153,11 +135,11 @@ class Solver(SolverBase):
         with self.b2.localForm() as loc_2:
             loc_2.set(0)
         assemble_vector(self.b2, self.L2)
-        apply_lifting(self.b2, [self.a2], [bcp_d])
+        apply_lifting(self.b2, [self.a2], [self.bcp_d])
         self.b2.ghostUpdate(
             addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE
         )
-        set_bc(self.b2, bcp_d)
+        set_bc(self.b2, self.bcp_d)
         self.solver2.solve(self.b2, self.p_sol.x.petsc_vec)
         self.p_sol.x.scatter_forward()
 
--- src/solvers/stabilized_schur.py
+++ src/solvers/stabilized_schur.py
@@ -3,14 +3,11 @@
 from typing import Callable
 from petsc4py import PETSc
 import numpy as np
-
-from basix.ufl import element
 from dolfinx.mesh import Mesh
 from dolfinx.fem import (
     DirichletBC,
     Constant,
     Function,
-    functionspace,
     form,
 )
 from ufl import (
@@ -55,39 +52,22 @@ class Solver(SolverBase):
         rho: float,
         mu: float,
         f: list,
-        h: list = None,
         initial_velocity: Callable[[np.ndarray], np.ndarray] = None,
     ):
-        self.mesh = mesh
-        self.dt = Constant(mesh, PETSc.ScalarType(dt))
-        self.rho = Constant(mesh, PETSc.ScalarType(rho))
-        self.mu = Constant(mesh, PETSc.ScalarType(mu))
-        self.f = Constant(mesh, PETSc.ScalarType(f))
-        if h == None:
-            self.h = Constant(mesh, PETSc.ScalarType([0.0] * mesh.geometry.dim))
-        else:
-            self.h = Constant(mesh, PETSc.ScalarType(h))
+        super().__init__(mesh, dt, rho, mu, f)
 
-        element_velocity = element(
-            "Lagrange",
-            mesh.topology.cell_name(),
-            1,
-            shape=(mesh.geometry.dim,),
+        super().initVelocitySpace(
+            "Lagrange", mesh.topology.cell_name(), 1, shape=(mesh.geometry.dim,)
         )
-        element_pressure = element("Lagrange", mesh.topology.cell_name(), 1)
+        super().initPressureSpace("Lagrange", mesh.topology.cell_name(), 1)
 
-        self.V = functionspace(mesh, element_velocity)
-        self.Q = functionspace(mesh, element_pressure)
         self.VQ = MixedFunctionSpace(self.V, self.Q)
 
         v, q = TestFunctions(self.VQ)
-
-        self.u_sol: Function = Function(self.V)
-        self.p_sol: Function = Function(self.Q)
-        self.u_prev, self.p_prev = Function(self.V), Function(self.Q)
+        self.u_prev = Function(self.V)
+        self.p_prev = Function(self.Q)
 
         if initial_velocity:
-            self.u_sol.interpolate(initial_velocity)
             self.u_prev.interpolate(initial_velocity)
 
         # weak form
@@ -101,7 +81,6 @@ class Solver(SolverBase):
         F += self.rho * dot(v, dot(u_mid, nabla_grad(u_mid))) * dx
         F -= inner(v, self.rho * self.f) * dx
         F += inner(self.epsilon(v), self.sigma(u_mid, p_sol, self.mu)) * dx
-        # F -= inner(v, self.h) * ds
         # probar p_prev en vez de p_sol
         F += dot(p_sol * n, v) * ds - dot(mu * nabla_grad(u_mid) * n, v) * ds
         F += inner(q, div(u_mid)) * dx
@@ -142,7 +121,7 @@ class Solver(SolverBase):
         F += F_pspg
         self.F = F
 
-    def updateSolution(self, x) -> None:
+    def updateSolution(self, x: PETSc.Vec) -> None:
         "Updates the solution functions u_sol and p_sol with the values in x."
         x.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
         self.u_sol.x.array[:] = x[: self.offset]
@@ -151,29 +130,37 @@ class Solver(SolverBase):
         self.p_sol.x.scatter_forward()
 
     def assembleJacobian(
-        self, snes, x, J_mat, P_mat, bcs: list[DirichletBC] = []
+        self,
+        snes: PETSc.SNES,
+        x: PETSc.Vec,
+        J_mat: PETSc.Mat,
+        P_mat: PETSc.Mat,
+        bcs: list[DirichletBC] = [],
     ) -> None:
         "Assembles the Jacobian matrix evaluated at u_sol and p_sol."
         J_mat.zeroEntries()
         assemble_matrix_block(J_mat, self.J_form, bcs)
         J_mat.assemble()
 
-    def assembleResidual(self, snes, x, F_vec, bcs: list[DirichletBC] = []) -> None:
-        "Assembles the residual vector evaluated at u_sol and p_sol, applies lifting and set_bcs so that the constrained dofs are x_n - g."
+    def assembleResidual(
+        self,
+        snes: PETSc.SNES,
+        x: PETSc.Vec,
+        F_vec: PETSc.Vec,
+        bcs: list[DirichletBC] = [],
+    ) -> None:
+        "Assembles the residual vector evaluated at u_sol and p_sol, applies lifting and set_bcs so that the constrained dofs are = x_n - g."
         self.updateSolution(x)
         with F_vec.localForm() as F_local:
             F_local.set(0.0)
 
         assemble_vector_block(F_vec, self.F_form, self.J_form, bcs=bcs, x0=x, alpha=-1.0)
 
-    def assembleTimeIndependent(
-        self, bcu: list[BoundaryCondition], bcp: list[BoundaryCondition]
-    ) -> None:
+    def setup(self, bcu: list[BoundaryCondition], bcp: list[BoundaryCondition]) -> None:
         # create linealizated problem
         du, dp = TrialFunctions(self.VQ)
 
         J = derivative(self.F, (self.u_sol, self.p_sol), (du, dp))
-
         self.F_form = form(extract_blocks(self.F))
         self.J_form = form(extract_blocks(J))
 
@@ -194,16 +181,21 @@ class Solver(SolverBase):
         self.u_prev.interpolate(stokes_solver.u_sol)
         self.p_prev.interpolate(stokes_solver.p_sol)
 
-        bcu_d = [bc.getBC(self.V) for bc in bcu]
-        bcp_d = [bc.getBC(self.Q) for bc in bcp]
+        self.bcu_d = [bc.getBC(self.V) for bc in bcu]
+        self.bcp_d = [bc.getBC(self.Q) for bc in bcp]
 
         # newton solver
         snes = PETSc.SNES().create(self.mesh.comm)
         snes.setOptionsPrefix("nonlinear_")
         snes.setType("newtonls")
-        snes.setFunction(self.assembleResidual, f=self.b, kargs={"bcs": [*bcu_d, *bcp_d]})
+        snes.setFunction(
+            self.assembleResidual, f=self.b, kargs={"bcs": [*self.bcu_d, *self.bcp_d]}
+        )
         snes.setJacobian(
-            self.assembleJacobian, J=self.A, P=None, kargs={"bcs": [*bcu_d, *bcp_d]}
+            self.assembleJacobian,
+            J=self.A,
+            P=None,
+            kargs={"bcs": [*self.bcu_d, *self.bcp_d]},
         )
 
         # x is the initial guess for the newton iteration = solution at previous time step
@@ -218,7 +210,7 @@ class Solver(SolverBase):
         )
         self.x_n.assemble()
 
-        # gmres global solver with field split preconditioner
+        # gmres global solver with field split (schur) preconditioner
         ksp = snes.getKSP()
         ksp.setType("gmres")
         snes.computeJacobian(self.x_n, self.A)  # asemble A in order to set up PC
@@ -227,8 +219,8 @@ class Solver(SolverBase):
         pc = ksp.getPC()
         pc.setType("fieldsplit")
         pc.setFieldSplitType(PETSc.PC.CompositeType.SCHUR)
-        pc.setFieldSplitSchurFactType(PETSc.PC.SchurFactType.DIAG)  # temporal
-        pc.setFieldSplitSchurPreType(PETSc.PC.SchurPreType.SELFP)  # temporal
+        pc.setFieldSplitSchurFactType(PETSc.PC.SchurFactType.DIAG)
+        pc.setFieldSplitSchurPreType(PETSc.PC.SchurPreType.SELFP)
 
         IS = PETSc.IS
         fields = self.V.dofmap.index_map, self.Q.dofmap.index_map
@@ -246,7 +238,7 @@ class Solver(SolverBase):
         ksp_p.setType("cg")
         ksp_p.getPC().setType("ilu")
 
-        # snes.setMonitor(lambda snes, its, rnorm: print(f"Iter {its}: residual = {rnorm}"))
+        # snes.setMonitor(lambda snes, its, rnorm: print(f"Iter {its}: residual = {rnorm}")) if --debug ??
         snes.setFromOptions()
         snes.setUp()
         self.solver = snes
@@ -266,9 +258,9 @@ class Solver(SolverBase):
                 vectors=[vec_const], comm=self.mesh.comm
             )
 
-    def solveStep(self, bcu: list[DirichletBC], bcp: list[DirichletBC]):
+    def solveStep(self):
         if self.nullsp.test(self.A):
-            self.A.setNullSpace(self.nullsp)
+            self.A.setNullSpace(self.nullsp)  # esto deberia moverse a setup pero da error
 
         self.nullsp.remove(self.x_n)  # creo que se puede quitar?
 
